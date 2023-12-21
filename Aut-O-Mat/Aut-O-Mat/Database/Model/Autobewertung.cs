aa9c71858bcd12c32253757ae30612f0c1e6f7b6
florianpreuss <kontakt@florianpreuss.de>
2023-12-21 17:50:17 -0600

    implemented all data models

--- Aut-O-Mat/Aut-O-Mat/Database/Model/Autobewertung.cs
+++ Aut-O-Mat/Aut-O-Mat/Database/Model/Autobewertung.cs
@@ -1,6 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Aut_O_Mat.Database.Model;
 
 public class Autobewertung
 {
+    [Key]    
+    [MaxLength(11)]
+    public int AutobewertungId { get; set; }
     
+    [Required]
+    [MaxLength(11)]
+    public int AutomodellId { get; set; }
+    public Automodell Automodell { get; set; }
+    
+    [Required]
+    [MaxLength(11)]
+    public int BewertungskategorieId { get; set; }
+    public Bewertungskategorie Bewertungskategorie { get; set; }
+    
+    [Required]
+    [Range(3,2)]
+    public decimal Bewertung { get; set; }
 }
--- Aut-O-Mat/Aut-O-Mat/Database/Model/Karosserieform.cs
+++ Aut-O-Mat/Aut-O-Mat/Database/Model/Karosserieform.cs
@@ -5,13 +5,15 @@ namespace Aut_O_Mat.Database.Model;
 public class Karosserieform
 {
     [Key]
+    [MaxLength(11)]
     public int KarosserieformId { get; set; }
     
     [Required]
     [MaxLength(16)]
     public string Name { get; set; }
     
+    [MaxLength(255)]
     public string IconUrl { get; set; }
-
+    
     public List<Automodell> Automodelle { get; set; }
 }
