7654df9f2efbf8dbd92b31780117f23b68d1f3b7
davidpfeil <david@pfeil-gmund.de>
2023-12-21 16:34:38 +0100

    Added Karosserieform.cs and Automarke.cs

--- Aut-O-Mat/Aut-O-Mat/Database/Model/Automarke.cs
+++ Aut-O-Mat/Aut-O-Mat/Database/Model/Automarke.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Aut_O_Mat.Database.Model;
 
 public class Automarke
 {
+    [Key] 
+    public int AutomarkeId { get; set; }
     
+    [Required]
+    [MaxLength(16)]
+    public string Name { get; set; }
+    
+    public string? LogoUrl { get; set; }
+
+    public List<Automodell> Automodelle { get; set; }
 }
--- Aut-O-Mat/Aut-O-Mat/Database/Model/Karosserieform.cs
+++ Aut-O-Mat/Aut-O-Mat/Database/Model/Karosserieform.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Aut_O_Mat.Database.Model;
 
 public class Karosserieform
 {
+    [Key]
+    public int KarosserieformId { get; set; }
     
+    [Required]
+    [MaxLength(16)]
+    public string Name { get; set; }
+    
+    public string IconUrl { get; set; }
+
+    public List<Automodell> Automodelle { get; set; }
 }
